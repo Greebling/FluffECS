0ac0ab254725e4c33f7633925f2b110e3d6dc9dd
Greebling <56256331+Greebling@users.noreply.github.com>
2021-05-16 14:12:57 +0200

    * reworked TypeID to only hash the actual type name and nothing else, now giving the same hash for similarly named classes and structs even on msvc

--- include/FluffECS/TypeId.h
+++ include/FluffECS/TypeId.h
@@ -3,42 +3,86 @@
 #include <cstdint>
 #include <cstddef>
 #include <type_traits>
+#include <string_view>
 
 #if _MSC_VER && !__INTEL_COMPILER // yeah msvc getting the special treatment... (https://en.cppreference.com/w/cpp/language/operator_alternative)
+
 #include <ciso646>
+
 #endif
 
 namespace flf
 {
 	using IdType = std::uint32_t;
-	using MultiIdType = std::uint32_t;
+	using MultiIdType = IdType;
 	
 	namespace internal
 	{
-		constexpr inline IdType
-		HashString(const char *str, std::size_t stringSize, std::uint32_t basis = 2166136261u) noexcept
+		template<typename T>
+		constexpr auto GetFuncName()
 		{
-			if (stringSize == 0)
+#if defined __clang__ || defined __GNUC__
+			return __PRETTY_FUNCTION__;
+#elif _MSC_VER && !__INTEL_COMPILER
+			return __FUNCSIG__;
+#else // not compatible with other compilers
+			return "";
+#endif
+		}
+		
+		template<typename T>
+		constexpr auto GetTypeName()
+		{
+#if defined __clang__ || defined __GNUC__
+			constexpr std::string_view str = GetFuncName<T>();
+			
+			if constexpr(str.size() < 55)
+			{
+				return std::string_view();
+			} else
+			{
+				return std::string_view(str.substr(54, str.length() - 55));
+			}
+#elif _MSC_VER && !__INTEL_COMPILER
+			constexpr std::string_view str = GetFuncName<T>();
+			
+			if constexpr(str.size() < 53)
+			{
+				return std::string_view();
+			} else
+			{
+				// MSVC gives extra info, whether type is a class or struct, giving as an additional unnecessary space at the beginning if it is a struct
+				if constexpr (str[46] == ' ')
+				{
+					return std::string_view(str.substr(47, str.length() - 54));
+				} else
+				{
+					return std::string_view(str.substr(46, str.length() - 53));
+				}
+			}
+#else
+			static_assert(false, "Compiler does not support conversion from type to string.");
+			return std::string_view();
+#endif
+		}
+		
+		constexpr inline IdType HashString(std::string_view str, IdType basis = 2166136261u) noexcept
+		{
+			if (str.empty())
 			{
 				return basis;
 			} else
 			{
-				return HashString(str + 1, stringSize - 1, (basis xor (IdType) str[0]) * 16777619u);
+				auto currChar = (IdType) ((unsigned char) str.front());
+				return HashString(str.substr(1, str.length() - 1), (basis xor currChar) * 16777619u);
 			}
 		}
 		
 		/// Creates a unique IdType for every type
 		template<typename T>
-		constexpr inline IdType TypeIdGenerator() noexcept
+		constexpr inline IdType GenerateTypeId() noexcept
 		{
-#if defined __clang__ || defined __GNUC__
-			return HashString(__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__));
-#elif _MSC_VER && !__INTEL_COMPILER
-			return HashString(__FUNCSIG__, sizeof(__FUNCSIG__));
-#else // not compatible with other compilers
-			static_assert(false, "constexpr TypeID is not supported by your Compiler! Choose either Clang, GCC or MSVC");
-			return 0;
-#endif
+			return HashString(GetTypeName<T>());
 		}
 	}
 	
@@ -46,7 +90,7 @@ namespace flf
 	template<typename T>
 	constexpr IdType TypeId()
 	{
-		return internal::TypeIdGenerator<T>();
+		return internal::GenerateTypeId<T>();
 	}
 	
 	/// Gives the same id for any combination of the same Ts
@@ -57,8 +101,7 @@ namespace flf
 		if constexpr(sizeof...(Ts) == 0)
 		{
 			return 0;
-		}
-		else if constexpr(sizeof...(Ts) == 1)
+		} else if constexpr(sizeof...(Ts) == 1)
 		{
 			return MultiTypeId<>() xor (..., TypeId<Ts>()); // trick to simply return TypeId<T>()
 		} else
@@ -95,12 +138,12 @@ namespace flf
 		{
 			MultiIdType result = MultiTypeId<>();
 			
-			for(;begin1 < end1; begin1++)
+			for (; begin1 < end1; begin1++)
 			{
 				result = result xor *begin1;
 			}
 			
-			for(;begin2 < end2; begin2++)
+			for (; begin2 < end2; begin2++)
 			{
 				result = result xor *begin2;
 			}
