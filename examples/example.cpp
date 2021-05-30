fe8748fc73dcddfa9d12124abf2b8804490c0f90
Greebling <56256331+Greebling@users.noreply.github.com>
2021-05-30 13:22:59 +0200

    * updated example and added info comments

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,8 +1,9 @@
-#include "../include/World.h"
+#include <iostream>
+#include <FluffECS/World.h>
 
 struct PositionData
 {
-	float x,y,z;
+	float x, y, z;
 };
 
 struct VelocityData
@@ -12,21 +13,37 @@ struct VelocityData
 
 int main()
 {
-	int numberOfEntities = 1024;
-	fluff:World myWorld;
-	myWorld.AddMultiple<PositionData, VelocityData>(numberOfEntities, {4.f,2.f,0.f}, {1.f,0,0});
+	flf::World myWorld{};
 	
+	// create many entities with the same components at once
+	int numberOfEntities = 1024;
+	myWorld.CreateMultiple<PositionData, VelocityData>(numberOfEntities, {4.f, 2.f, 0.f}, {1.f, 0, 0});
+	
+	// iterate over all entities with specific components
 	float deltaTime = 1.f / 60.f;
-	myWorld.Foreach<PositionData, VelocityData>(
-			[deltaTime](PositionData &position, const VelocityData &velocity)
+	myWorld.Foreach<PositionData &, VelocityData>( // both reference and value semantics are supported for increased performance!
+			[deltaTime](PositionData &position, VelocityData velocity)
 			{
 				position.x += velocity.dx * deltaTime;
 				position.y += velocity.dy * deltaTime;
 				position.z += velocity.dz * deltaTime;
 			}
+	);
+	// can also get the entities ID for additional information
+	myWorld.ForeachEntity<PositionData&>(
+			[](flf::EntityId id, auto position) // can also use auto types for parameters
+			{
+				position.x += ((float) id) * 0.01f;
+			}
 			);
 	
-	fluff:EntityWith<PositionData, VelocityData> addedEntity = myWorld.Add();
-	PositionData &position = myWorld.Get<PositionData>(addedEntity);
-	myWorld.Remove(addedEntity);
+	
+	
+	flf::Entity addedEntity = myWorld.CreateEntity<PositionData>();
+	if(PositionData* position = addedEntity.Get<PositionData>(); position != nullptr)
+	{
+		std::cout << "Entity is at " << position->x << " | " << position->y << " | " << position->z << "\n";
+	}
+	auto &position = myWorld.Get<PositionData>(addedEntity); // get specific components of entities
+	addedEntity.Destroy(); // destroys the entity and all of its components
 }
