62e8e686331b49ec61b3d80a6b698cf83e7f8f0f
Greebling <56256331+Greebling@users.noreply.github.com>
2021-08-16 20:08:30 +0200

    + object-oriented example added

--- examples/example.cpp
+++ examples/example.cpp
@@ -11,6 +11,7 @@ struct VelocityData
 	float dx, dy, dz;
 };
 
+
 int main()
 {
 	flf::World myWorld{};
@@ -18,6 +19,7 @@ int main()
 	// create many entities with the same components at once
 	int numberOfEntities = 1024;
 	myWorld.CreateMultiple<PositionData, VelocityData>(numberOfEntities, {4.f, 2.f, 0.f}, {1.f, 0, 0});
+	
 	
 	// iterate over all entities with specific components
 	float deltaTime = 1.f / 60.f;
@@ -32,6 +34,7 @@ int main()
 			}
 	);
 	
+	
 	// can also get the entities ID for additional information
 	myWorld.ForeachEntity(
 			// using auto types for parameters is of course also possible
@@ -39,15 +42,31 @@ int main()
 			{
 				position.x += ((float) id) * 0.01f;
 			}
-			);
+	);
 	
 	
+	// using the object-oriented way to write systems is also possible
+	struct VelocityApplySystem
+	{
+		float deltaTime = 1.0f;
+		
+		void operator()(PositionData &position, const Velocity VelocityApplySystem) const
+		{
+			position.x += velocity.dx * deltaTime;
+			position.y += velocity.dy * deltaTime;
+			position.z += velocity.dz * deltaTime;
+		}
+	} velocitySystem{1.0f};
+	myWorld.Foreach(velocitySystem); // do note that this does create a copy of velocitySystem
 	
+	
+	// you can also get components of individual entities and check for a components existence
 	flf::Entity addedEntity = myWorld.CreateEntity<PositionData>();
-	if(PositionData* position = addedEntity.Get<PositionData>(); position != nullptr)
+	if (PositionData *position = addedEntity.Get<PositionData>(); position != nullptr)
 	{
 		std::cout << "Entity is at " << position->x << " | " << position->y << " | " << position->z << "\n";
 	}
+	
 	
 	addedEntity.Destroy(); // destroys the entity and all of its components
 }
